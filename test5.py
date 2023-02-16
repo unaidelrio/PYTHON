1875d428efdc0012aa97822567e2fabed16052e8
unaidelrio <124903963+unaidelrio@users.noreply.github.com>
2023-02-16 17:45:03 +0100

    Update test5.py

--- test5.py
+++ test5.py
@@ -8,8 +8,10 @@ import turtle
 myTurtle = turtle.Turtle()
 myWin = turtle.Screen()
 
-for i in range(4):
+for i in range(18):
     myTurtle.forward(20)
-    myTurtle.right(90)
+    myTurtle.right(50)
+    myTurtle.left(30)
+   
 
 myWin.exitonclick()

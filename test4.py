c7e972dd201645ae59e755bf90c83220aad21754
unaidelrio <124903963+unaidelrio@users.noreply.github.com>
2023-02-16 16:27:04 +0000

    a

--- test4.py
+++ test4.py
@@ -1,11 +1,9 @@
+# from urllib.request import urlopen
 
+# page= urlopen("http://info.cern.ch/")
+# content = page. read()
 
-from urllib.request import urlopen
-
-page= urlopen("http://info.cern.ch/")
-content = page. read()
-
-print(content)
+# print(content)
 
 import requests
 
